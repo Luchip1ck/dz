f5ce70b8df89b5de02f3420556f450442e27b1dd
Еренбаев Нурлибек Нурлан улы <ErenbaevN@lcoast.itstep>
2024-12-11 18:04:44 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿/*string name = Console.ReadLine();
+Console.WriteLine("Здраствуйте " + name);*/
+
+/*int num1 = 123;
+int num2 = 321;
+int num3 = num1 + num2;
+Console.WriteLine("Сумма переменных " + num3);*/
+
+/*int num1 = 123;
+int num2 = 321;
+if (num1 > num2)
+{
+    Console.WriteLine(num1 - num2);
+}
+else
+{
+    Console.WriteLine(num2 - num1);
+}*/
+
+//int age = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Пользователь прожил: " + age + " лет/года");
+
+/*int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0)
+{
+    Console.WriteLine("Отрицательное");
+}
+else
+{
+    Console.WriteLine("Положительное");
+}*/
+
+/*int[] ints = new int[4] { 4, 5, 23, 46 };
+int sum = 0;
+for (int i = 0; i < ints.Length; i++)
+{
+    if (ints[i] %2 == 0)
+    {
+        sum += ints[i];
+    }
+}
+Console.WriteLine(sum);*/
+
+/*int[] ints = new int[4] {-4, -5, -23, -46 };
+int sum = 0;
+for (int i = 0; i < ints.Length; i++)
+{
+    if (ints[i] > sum)
+    {
+        sum = ints[i];
+    }
+}
+    Console.WriteLine(sum);*/
+
+/*int[] ints = new int[4] { -4, -5, -23, -46 };
+int sum = 0;
+int min = 10000;
+int max = -10000;
+for (int i = 0; i < ints.Length; i++)
+{
+    if (ints[i] > max)
+    {
+        max = ints[i];
+    }
+    if (ints[i] < min)
+    {
+        min = ints[i];
+    }
+}
+Console.WriteLine(max + min);*/
